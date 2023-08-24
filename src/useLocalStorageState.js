698b4d84d9c3f3e8c104218741f52baa3fcfdaf1
Deepjyoti23 <deepjyoti0401@gmail.com>
2023-08-24 19:10:26 +0530

    Final

---
+++ src/useLocalStorageState.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorageState(initialstate, key) {
+  const [value, setValue] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return storedValue ? JSON.parse(storedValue) : initialstate;
+  });
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(value));
+    },
+    [value, key]
+  );
+
+  return [value, setValue];
+}
